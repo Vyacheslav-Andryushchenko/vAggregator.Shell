de158316d99ccb60df9a992689475c098b4061d5
slavaxx90 <slavaxx90@gmail.com>
2017-04-23 22:01:57 +0600

    Added Shell.Common assembly.

---
+++ vAggregator.Shell.Common/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace vAggregator.Shell.Common.Models
+{
+    /// <summary>
+    /// This class represents the user model.
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string LoginName { get; set; }
+
+        public string EMail { get; set; }
+    }
+}
