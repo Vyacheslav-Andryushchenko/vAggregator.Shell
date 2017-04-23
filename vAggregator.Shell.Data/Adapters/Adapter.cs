a175f55de21a233b3c39922e6da50a5742f432ad
slavaxx90 <slavaxx90@gmail.com>
2017-04-24 00:25:19 +0600

    Writes few comments to Shell.Data assembly.

--- vAggregator.Shell.Data/Adapters/Adapter.cs
+++ vAggregator.Shell.Data/Adapters/Adapter.cs
@@ -3,10 +3,29 @@ using vAggregator.Shell.Data.Entities;
 
 namespace vAggregator.Shell.Data.Adapters
 {
+    /// <summary>
+    /// Represents the adapter is used to adapt entities into models.
+    /// </summary>
+    /// <typeparam name="TSource">Entity type.</typeparam>
+    /// <typeparam name="TTarget">Model type.</typeparam>
+    /// <remarks>
+    /// The class is used to preventing cross-layer interaction
+    /// with entities which located in ORM context.
+    /// </remarks>
     internal abstract class Adapter<TSource, TTarget> where TSource : IEntity where TTarget : class
     {
+        /// <summary>
+        /// Adapts entity into model.
+        /// </summary>
+        /// <param name="source">Entity.</param>
+        /// <returns>Model.</returns>
         public abstract TTarget Adapt(TSource source);
 
+        /// <summary>
+        /// Adapts enumeration of entities to enumeration of models.
+        /// </summary>
+        /// <param name="source">Enumeration of entities.</param>
+        /// <returns>Enumeration of models.</returns>
         public IEnumerable<TTarget> AdaptRange(IEnumerable<TSource> source)
         {
             var result = new List<TTarget>();
--- vAggregator.Shell.Data/Adapters/UsersAdapter.cs
+++ vAggregator.Shell.Data/Adapters/UsersAdapter.cs
@@ -3,6 +3,9 @@ using vAggregator.Shell.Common.Models;
 
 namespace vAggregator.Shell.Data.Adapters
 {
+    /// <summary>
+    /// Represents the adapter is used to adapt users into user models.
+    /// </summary>
     internal class UsersAdapter : Adapter<User, UserModel>
     {
         public override UserModel Adapt(User source)
